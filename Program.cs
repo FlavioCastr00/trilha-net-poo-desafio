9250fd318ff113d0a9d4e41374e8fdbfd9fcbdd3
Flavio Henrique Mezzaroba de Castro <flaviohen1701@gmail.com>
2024-01-02 09:03:51 -0300

    Desafio Concluído git status!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// IMPLEMENTADO
+Console.WriteLine("Para o Iphone:");
+Iphone iphone = new Iphone(numero: "123456789", modelo: "Modelo 2", imei: "111111", memoria: 120);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\nPara o Nokia:");
+Nokia nokia = new Nokia(numero: "987654321", modelo: "Modelo 5", imei:"444444", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
